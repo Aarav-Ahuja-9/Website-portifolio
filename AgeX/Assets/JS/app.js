fb79093634c98f809913526847b87abcaa4f03cc
Aarav-Ahuja-9 <mr.aaravahuja@gmail.com>
2025-10-14 19:19:54 +0530

    now its more usable than ever

--- AgeX/Assets/JS/app.js
+++ AgeX/Assets/JS/app.js
@@ -11,17 +11,31 @@ let currMonth = date.getMonth() + 1;
 let currYear = date.getFullYear();
 
 checkBtn.addEventListener('click', () => {
-
-
-    let ageYearNum = new Number(year.value).toFixed(0);
+    let ageYearNum = new Number(year.value);
     let ageYear = currYear - ageYearNum.valueOf();
 
-    let ageDayNum = new Number(day.value).toFixed(0);
+    let ageDayNum = new Number(day.value);
     let ageDay = currDay - ageDayNum.valueOf();
 
-    let ageMonthNum = new Number(month.value).toFixed(0);
+    let ageMonthNum = new Number(month.value);
     let ageMonth = currMonth - ageMonthNum.valueOf();
 
+    ageSummary = `You are ${ageYear} years, ${ageMonth} months and ${ageDay} days old`
 
-    infoText.innerText = `You are ${ageYear} years, ${ageMonth} months and ${ageDay} days old`;
+    if (ageDay < 0) {
+        ageMonth--;
+        ageDay = currDay + ageDayNum.valueOf()
+    }
+
+    if (ageMonth < 0) {
+        ageYear--;
+        ageMonth = 12 + ageMonth;
+    }
+
+    if (ageYear < currYear) {
+        ageSummary = `Your age year cannot be greater than current year`
+
+    }
+
+    infoText.innerText = ageSummary;
 })
